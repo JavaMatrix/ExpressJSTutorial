bb5278f30dee4b18d38f782a0f9af3a9198249d2
Nathanael Page <defensoft@gmail.com>
2024-04-03 02:46:21 -0400

    Formatting

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ const asyncHandler = require("express-async-handler");
 //@route GET /api/contacts
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Page to get all contacts, using router."});
+    res.status(200).json({ message: "Page to get all contacts, using router." });
 });
 
 //@desc Create a new contact
@@ -12,33 +12,33 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 const createContact = asyncHandler(async (req, res) => {
     console.log(req.body);
-    const {name, email, phone} = req.body;
+    const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         res.status(400)
         throw new Error("All fields are mandatory!");
     }
-    res.status(201).json({message: "Page to create new contacts, using router."});
+    res.status(201).json({ message: "Page to create new contacts, using router." });
 });
 
 //@desc Get an individual contact
 //@route GET /api/contacts/:id
 //@access public
 const getIndividualContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Page to get contact ${req.params.id}, using router.`});
+    res.status(200).json({ message: `Page to get contact ${req.params.id}, using router.` });
 });
 
 //@desc Update a contact by ID
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Page to update ${req.params.id}, using router.`});
+    res.status(200).json({ message: `Page to update ${req.params.id}, using router.` });
 });
 
 //@desc Delete a contact by ID
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Page to delete ${req.params.id}, using router.`});
+    res.status(200).json({ message: `Page to delete ${req.params.id}, using router.` });
 });
 
-module.exports = {getContact, createContact, getIndividualContact, updateContact, deleteContact};
+module.exports = { getContact, createContact, getIndividualContact, updateContact, deleteContact };
